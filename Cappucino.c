f8b686d8d2ed167569e0dedc90484f2ff5c078c2
Naveen <117004641+Naveen20053@users.noreply.github.com>
2024-09-25 18:23:18 +0400

    csci291 lab 2

--- Cappucino.c
+++ Cappucino.c
@@ -6,3 +6,32 @@ int main()
 {
 
 }
+void expresso_coffee()
+{}
+
+int main()
+{
+
+}
+void mocha()
+{}
+
+int main()
+{
+
+}
+#define espresso_coffee, 8
+#define cappucino_coffee_gram 8 
+#define mocha_gram 8
+#define espresso_water 30
+#define cappucino_water 30
+#define mocha_water 39
+#define espresso_milk 0
+#define cappucino_milk 70
+#define mocha_milk 160
+#define espresso_chocolate_syrup 0
+#define cappucino_chocolate_syrup 0
+#define mocha_chocolate_syrup 30
+#define espresso_price 3.5
+#define cappucino_price 4.5
+#define mocha_price 5.5
